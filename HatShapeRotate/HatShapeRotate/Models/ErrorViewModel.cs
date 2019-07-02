821b01c7c1efd2c63aaedc6621ad727ad426dbc3
Cam Collins <33230098+ccollins63@users.noreply.github.com>
2019-07-02 12:00:02 -0400

    Add files via upload

---
+++ HatShapeRotate/HatShapeRotate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HatShapeRotate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
